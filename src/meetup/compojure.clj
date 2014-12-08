7ad8843bb5e3f11dd6a675358697bdf9302de6fb
Guillaume Buisson <guillaume@vigiglobe.com>
2014-12-08 14:37:03 +0100

    fixed compojure sample

--- src/meetup/compojure.clj
+++ src/meetup/compojure.clj
@@ -9,17 +9,19 @@
 (defonce orders (atom {}))
 
 (def recipes #{:hawaian :italian :vegan})
-      
+
 (def max-count 5)
-        
+
 
 (defn handle-order [pizza nb]
-  (cond (not (contains? pizza recipe)) {:status 404 :body "the requested recipe does not exists"}
+
+  (cond (not (some #{(keyword pizza)} recipes)) {:status 404 :body "the requested recipe does not exists"}
         (> nb max-count) {:status 503 :body "you ordered too many pizzas"}
-        :else (do (swap! orders into {(keyword (str (java.util.UUID/randomUUID))) p})
+        :else (do (swap! orders into {(keyword (str (java.util.UUID/randomUUID))) {:pizza pizza
+                                                                                   :count nb}})
                   {:status 200
                    :body "your pizza will be available shortly"})))
-      
+
 (defn cancel-pizza-handler [params]
   (let [id (:id (keywordize-keys params))]
     (if-not (nil? (get @orders (keyword id)))
@@ -30,7 +32,7 @@
          :body "your order does not exists"})))
 
 (defroutes handler
-  (POST "/order/:pizza" {{:nb nb} :params} (handle-order pizza (read-string nb)))
+  (POST "/order/:pizza" [pizza nb] (handle-order pizza (read-string nb)))
   (POST "/cancel/pizza" {params :params} (cancel-pizza-handler params))
 
   (route/not-found "<h1>Invalid route.</h1>"))
